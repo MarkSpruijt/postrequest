dca09839b3f80206bce500d4d39e9a5c2a6b115f
sandrr26 <sandersteehouwer@hotmail.com>
2015-05-12 15:05:53 +0200

    Fixed the Routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,13 +1,9 @@
 <?php
 
 Route::controllers([
-<<<<<<< HEAD
 	'/question'	=> 'QuestionController',
+	'account' => 'UserController',
 
 	//This Route needs to stay at the BOTTOM!!!!!!! Or everything will DIE!!!
-	'/' => 'HomeController' 
-=======
-	'account' => 'UserController',
-	'/' => 'HomeController'
->>>>>>> dc8af57ecd9e6e1fe9de505a5dcfd2b0d3b7bd5b
+	'/' => 'HomeController' 	
 ]);
