060d638be4fda73db31bff966a16a16cd8c9a187
Mark Spruijt <me@nightduty.nl>
2015-05-12 15:16:19 +0200

    Added Answers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,17 +1,18 @@
 <?php namespace App\Http\Controllers;
 
-
+use App\Models\Answer;
 
 class HomeController extends Controller {
-
-	public function __construct()
-	{
-		$this->middleware('guest');
-	}
 
 	public function getIndex()
 	{
 		return view('home');
+	}
+
+	public function getAnswer($id = 1){
+		$answers = Answer::where('question_id', $id)->get();
+		
+		return view('answer.show')->with('answers', $answers);
 	}
 
 }
--- database/migrations/2015_05_12_073354_create_answervotes_table.php
+++ database/migrations/2015_05_12_073354_create_answervotes_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAnswersTable extends Migration {
+class CreateAnswervotesTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,13 +12,13 @@ class CreateAnswersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('answers', function(Blueprint $table)
+		Schema::create('answer_votes', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->integer('user_id');
-			$table->integer('question_id');
-			$table->text('content');
-			$table->timestamps();
+			$table->integer('answer_id');
+			$table->boolean('vote'); // 0 = negative, 1 = positive;
+			$table->timestamp('created_at');
 		});
 	}
 
@@ -29,7 +29,7 @@ class CreateAnswersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('answers');
+		Schema::drop('answer_votes');
 	}
 
 }
