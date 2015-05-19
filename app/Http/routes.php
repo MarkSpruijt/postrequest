916828b4a04f12f7c08f88dd11361952e267ff55
Mark Spruijt <me@nightduty.nl>
2015-05-19 09:10:33 +0200

    Votes are now counted in the model

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,7 +3,6 @@
 Route::get('/','QuestionController@getIndex');
 Route::get('question/{question_id}', 'QuestionController@getDetails');
 
-Route::get('logout', 'UserController@getLogout');
 Route::get('question/{question_id}/{answer_id}/comment', 'CommentController@getCreate');
 Route::post('question/{question_id}/{answer_id}/comment', 'CommentController@postCreate');
 Route::get('question/{question_id}/{answer_id}/choose', 'QuestionController@chooseAnswer');
