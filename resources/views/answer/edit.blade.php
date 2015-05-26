635497e03fedaa6a078b4d995b04f357f0e14254
Sebasdc <sebastiaandeconinck10@hotmail.com>
2015-05-26 10:32:33 +0200

    Minor improvement

--- resources/views/answer/edit.blade.php
+++ resources/views/answer/edit.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 	<h1>Antwoord bewerken</h1>
 	{!!Form::open()!!}
-	{!!Form::label('Antwoord')!!}
-	{!!Form::text('content', $answer->content)!!}
+	{!!Form::label('Antwoord')!!}<br/>
+	{!!Form::textarea('content', $answer->content)!!}
 	{!!Form::submit('Wijzigingen opslaan')!!}
 	{!!Form::close()!!}
 @endsection
