45445b4aaf89ddbdacbdc3409ba9ea136daf1943
sandrr26 <sandersteehouwer@hotmail.com>
2015-06-02 11:17:42 +0200

    Databaseeder now includes the Username

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class UserTableSeeder extends Seeder {
 
 	public function run()
 	{
-		User::create(['realname' => 'Docent A','email' => 'docent@mydavinci.nl', 'password' => Hash::make('docent'), 'rank' => 100]);
-		User::create(['realname' => 'Leerling A','email' => 'leerling@mydavinci.nl', 'password' => Hash::make('leerling')]);
+		User::create(['realname' => 'Docent A','username' => 'Docent', 'email' => 'docent@mydavinci.nl', 'password' => Hash::make('docent'), 'rank' => 100]);
+		User::create(['realname' => 'Leerling A','username' => 'Leerling','email' => 'leerling@mydavinci.nl', 'password' => Hash::make('leerling')]);
 	}
 }
