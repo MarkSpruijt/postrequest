a7151e99ea648754dc56b6e6a44f1e8e3173739f
sandrr26 <sandersteehouwer@hotmail.com>
2015-06-11 08:55:37 +0200

    OOps

---
+++ app/Http/Controllers/AnswerController.php
@@ -0,0 +1,75 @@
+<?php namespace App\Http\Controllers;
+
+use Auth;
+use App;
+use App\Models\Answer;
+use App\Models\AnswerVote;
+use Illuminate\Http\Request;
+
+class AnswerController extends Controller {
+
+	public function getIndex($id = 1) {
+
+		$answers = Answer::where('question_id', $id)->get();
+
+		return view('answer.show')->with('answers', $answers);
+	}
+
+	public function getCreate() {
+
+		return view('answer.add');
+	}
+
+	public function postCreate(Request $request, $id){
+
+		$data = $request->only('content');
+		$user_id = Auth::user()->id;
+		Answer::create(['content' => $data['content'],'question_id' => $id, 'user_id' => $user_id]);
+
+		return redirect("question/". $id);
+	}
+
+	public function getEdit($id) {
+
+		$answer = Answer::find($id);
+
+		if($answer && $answer->user_id == Auth::user()->id)
+			return view('answer.edit')->withAnswer($answer);
+
+		return App::abort(403);
+	}
+
+	public function postEdit(Request $request, $id)	{
+
+		$answer = Answer::find($id);
+		$data = $request->only('content');
+
+		if($answer && $answer->user_id == Auth::user()->id)
+		{
+			$answer->content = $data['content'];
+			$answer->save();
+			return redirect('question/' . $answer->question_id);
+		}
+
+		return App::abort(403);
+
+	}
+
+	public function getVote($id,$votenumber = 1){
+
+		$user_id = Auth::user()->id;
+		
+		// Did the user vote already?
+		if(AnswerVote::where('user_id', $user_id)->where('answer_id', $id)->first()){
+			return redirect()->back();
+		}
+
+		$vote = new AnswerVote;
+		$vote->user_id = $user_id;
+		$vote->answer_id = $id;
+		$vote->vote = $votenumber;
+		$vote->save();
+
+		return redirect()->back();
+	}
+}
